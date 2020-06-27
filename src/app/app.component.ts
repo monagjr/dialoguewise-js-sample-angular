117482997c866bacdbbf0468932f3f89a9523aff
MO. Jr <monagjr@gmail.com>
2020-06-27 21:01:31 +0000

    Sample Integration with DialogueWise JS Package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DialogueWiseService, DialogueWiseRequest } from 'dialoguewise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample';
+  heroContent = '';
+
+  async ngOnInit(): Promise<void>{
+    const request: DialogueWiseRequest = {
+      slug: 'hero-section',
+      apiKey: 'b1266377591c4f2a9494c3abdd2cac5381D6Z825D26CEBAE8B6rn',
+      emailHash: '/kgmM46s1xC56BOFWRZp4j+0bdU19URpXdNT9liAX50=',
+    };
+
+    const dialogueWiseService = new DialogueWiseService();
+    const res = await dialogueWiseService.getDialogue(request);
+    this.heroContent = res.dialogue[0]['hero-content'];
+  }
 }
